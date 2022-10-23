9146faac93c8c4d5e89ead64094a4692d3c9d0e6
Davyd Kohut <davyd.kohut@gmail.com>
2022-10-23 21:50:17 +0300

    docs introduction

--- pages/docs/index.vue
+++ pages/docs/index.vue
@@ -1,3 +1,11 @@
+<script setup>
+const life = `
+дія життя(а, б, в, г)
+  життя(а(г), б(г), в(г), г)
+кінець
+`.trim();
+</script>
+
 <template>
   <DocsWrapper>
     <h1 class="docs-content-title">
@@ -5,14 +13,24 @@
     </h1>
 
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad adipisci animi aperiam assumenda aut beatae
-      blanditiis dolor ducimus earum eius eligendi error esse est eum eveniet, excepturi explicabo harum, hic illo
-      in ipsam iusto laboriosam minus molestias mollitia neque nisi nobis officia officiis placeat quae rerum,
-      sapiente sint temporibus totam ut voluptas. Alias amet ipsam itaque libero minus necessitatibus numquam,
-      possimus praesentium qui quibusdam quod, repudiandae soluta suscipit temporibus vero. Adipisci amet assumenda
-      at aut commodi consectetur, corporis delectus distinctio eveniet ex expedita fuga impedit in magni
-      necessitatibus nobis numquam perspiciatis, placeat quam quibusdam, quidem reiciendis saepe sapiente tenetur
-      voluptate.
+      <span class="diia-word">Дія</span> є простою, зручною та динамічною мовою програмування на основі кирилічної
+      абетки.
     </p>
+    <p>
+      Метою створення <span class="diia-word">Дії</span> є <a href="https://github.com/kohutd">моє</a> бажання до
+      написання комп'ютерних команд українською мовою.
+    </p>
+    <p>
+      Я вірю, що кожен українець має право використовувати зрозумілі йому інструменти в щоденному житті.
+    </p>
+    <div class="code-window code-window-full">
+      <ClientOnly>
+        <highlightjs
+            language="diia"
+            :autodetect="false"
+            :code="life"
+        />
+      </ClientOnly>
+    </div>
   </DocsWrapper>
 </template>
