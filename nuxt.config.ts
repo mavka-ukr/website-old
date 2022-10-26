bf0560fca5bd39d5c206cb9a813c79d3deb205d8
Davyd Kohut <davyd.kohut@gmail.com>
2022-10-26 08:33:26 +0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,21 @@
 export default defineNuxtConfig({
     meta: {
         title: 'Дія - сучасна мова програмування',
+        link: [
+            {
+                rel: 'preconnect',
+                href: 'https://fonts.googleapis.com',
+            },
+            {
+                rel: 'preconnect',
+                href: 'https://fonts.gstatic.com',
+                crossorigin: '',
+            },
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css2?family=Gentium+Book+Plus:ital,wght@0,400;0,700;1,400;1,700&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
+            },
+        ],
     },
     modules: [
         '@nuxt/content'
