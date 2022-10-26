5b275cb2db4fa15435702a791e7c80fd2425e851
Davyd Kohut <davyd.kohut@gmail.com>
2022-10-26 03:21:39 +0300

    fix???

--- plugins/rgb_vars.client.js
+++ plugins/rgb_vars.client.js
@@ -1,5 +1,25 @@
-import { generateRgbaVars } from "~/utils/color.js";
 import { defineNuxtPlugin } from "nuxt/app";
+
+function hexToRgb(hex) {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+function generateRgbaVars(name) {
+    const $root = document.getElementsByTagName('html')[0];
+    const colorValue = getComputedStyle($root).getPropertyValue(name);
+
+    if (colorValue) {
+        const colorRgb = hexToRgb(colorValue.trim());
+
+        $root.style.setProperty(`${name}_rgb`, `${colorRgb.r},${colorRgb.g},${colorRgb.b}`);
+    }
+}
 
 export default defineNuxtPlugin(() => {
     generateRgbaVars('--bg-color');
