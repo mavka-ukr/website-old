74ef408488408d32c8b2cd59d8a588970bc8a57f
Davyd Kohut <davyd.kohut@gmail.com>
2023-02-16 01:55:25 +0200

    upd

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script setup>
+onBeforeMount(() => {
+  if (window.location.hostname !== 'localhost' && window.location.hostname !== 'мавка.укр') {
+    window.location.href = 'https://мавка.укр';
+  }
+});
+</script>
+
 <template>
   <NuxtPage />
 </template>
