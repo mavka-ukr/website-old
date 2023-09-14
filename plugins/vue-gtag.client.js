2ef7edc5158c287f5e96f07a5d6cabff77e458f0
Davyd Kohut <expexes@gmail.com>
2023-09-14 21:24:57 +0300

    upd

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -1,10 +1,12 @@
-import VueGtag from 'vue-gtag-next'
+import VueGtag from "vue-gtag-next";
 import { defineNuxtPlugin } from "nuxt/app";
 
 export default defineNuxtPlugin((nuxtApp) => {
+  if (window.location.hostname !== "localhost") {
     nuxtApp.vueApp.use(VueGtag, {
-        property: {
-            id: 'G-YE5CHW62C9'
-        }
+      property: {
+        id: "G-YE5CHW62C9",
+      },
     });
+  }
 });
