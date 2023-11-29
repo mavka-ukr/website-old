6d59bfc566b229e7d59874fcdd9ec86fca73689a
Davyd Kohut <davyd.kohut@gmail.com>
2023-11-29 06:03:18 +0200

    🌠

--- plugins/hjs.client.js
+++ plugins/hjs.client.js
@@ -24,91 +24,95 @@ const diia = () => {
       keyword:
         "та ціль макет субʼєкт це дія рівно більше менше макет пак спробувати містить не я js файл впасти тривала зловити є перебрати кінець структура чекати взяти якщо або і інакше вернути поки як дати модуль пропустити зупинити",
       literal:
-        "так ні пусто текст логічне число список словник словник_м список_м текст_м число_м логічне_м обʼєкт ніщо щось []ніщо []щось []текст []логічне []число []список []словник",
+        "так ні пусто текст логічне число список словник словник_м список_м текст_м число_м логічне_м обʼєкт ніщо щось []ніщо []щось []текст []логічне []число []список []словник"
     },
     unicodeRegex: true,
     contains: [
       {
         begin: ["дія", /\(/],
-        className: { 0: "keyword" },
+        className: { 0: "keyword" }
       },
       {
         begin: [/[a-zа-яіїє_]/, /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/, /\(/],
-        className: { 1: "title.function", 2: "title.function" },
+        className: { 1: "title.function", 2: "title.function" }
       },
       {
         begin: [/[A-ZА-ЯІЇЄ]/, /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/, /\(/],
-        className: { 1: "title.class", 2: "title.class" },
+        className: { 1: "title.class", 2: "title.class" }
       },
       {
         begin: [" ", /[A-ZА-ЯІЇЄ]/, /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/],
-        className: { 2: "title.class", 3: "title.class" },
+        className: { 2: "title.class", 3: "title.class" }
+      },
+      {
+        begin: ["  ", /[A-ZА-ЯІЇЄ]/, /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/],
+        className: {}
       },
       {
         begin: [/\[\]/, /[A-ZА-ЯІЇЄ]/, /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/],
-        className: { 2: "title.class", 3: "title.class" },
+        className: { 2: "title.class", 3: "title.class" }
       },
       {
         begin: [
           "структура ",
           /[a-zA-Zа-яА-ЯіІїЇєЄ_]/,
-          /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/,
+          /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/
         ],
-        className: { 2: "title.class", 3: "title.class" },
+        className: { 2: "title.class", 3: "title.class" }
       },
       {
         begin: [
           "макет модуль ",
           /[a-zA-Zа-яА-ЯіІїЇєЄ_]/,
-          /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/,
+          /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/
         ],
-        className: {},
+        className: {}
       },
       {
         begin: [
           "макет тривала дія ",
           /[a-zA-Zа-яА-ЯіІїЇєЄ_]/,
-          /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/,
+          /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/
         ],
-        className: { 2: "title.function", 3: "title.function" },
+        className: { 2: "title.function", 3: "title.function" }
       },
       {
         begin: [
           "макет ",
           /[a-zA-Zа-яА-ЯіІїЇєЄ_]/,
-          /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/,
+          /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/
         ],
-        className: { 2: "title.class", 3: "title.class" },
+        className: { 2: "title.class", 3: "title.class" }
       },
       {
         begin: [/[A-ZА-ЯІЇЄ]/, /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/, /\./],
-        className: { 1: "title.class", 2: "title.class" },
+        className: { 1: "title.class", 2: "title.class" }
       },
       {
         begin: ["дати ", /[A-ZА-ЯІЇЄ]/, /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/],
-        className: { 2: "title.class", 3: "title.class" },
+        className: { 2: "title.class", 3: "title.class" }
       },
       {
         begin: ["дати ", /[a-zа-яіїє_]/, /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/],
-        className: { 2: "title.function", 3: "title.function" },
+        className: { 2: "title.function", 3: "title.function" }
       },
       {
         begin: [
           "взяти ",
           /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ]+\./,
           /[a-zа-яіїє_]/,
-          /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ]*/,
+          /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ]*/
         ],
-        className: { 3: "title.function", 4: "title.function" },
+        className: { 3: "title.function", 4: "title.function" }
       },
       {
         begin: [
           "взяти ",
           /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ]+\./,
           /[A-ZА-ЯІЇЄ]/,
-          /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ]*/,
+          /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ]*/
         ],
-        className: { 3: "title.class", 4: "title.class" },
+        className: { 3: "title.class", 4: "title.class" }
       },
       {
         className: "string",
@@ -118,9 +122,9 @@ const diia = () => {
           {
             begin: /%\(/,
             end: /\)/,
-            scope: "subst",
-          },
-        ],
+            scope: "subst"
+          }
+        ]
       },
       {
         className: "string",
@@ -130,22 +134,30 @@ const diia = () => {
           {
             begin: /%\(/,
             end: /\)/,
-            scope: "subst",
-          },
-        ],
+            scope: "subst"
+          }
+        ]
       },
       hljs.COMMENT(";;;", ";;;"),
       hljs.COMMENT(";;", "$"),
       {
         scope: "number",
-        begin: /(-?)0ш([АБВГҐДабвгґд0-9])+/,
+        begin: /(-?)0ш([АБВГҐДабвгґд0-9])+/
       },
       {
         scope: "number",
-        begin: /(-?)0д([01])+/,
+        begin: /(-?)0д([01])+/
       },
       hljs.C_NUMBER_MODE,
-    ],
+      {
+        begin: [" ", /[0-9]+(.[0-9]+)?/, "..", /[0-9]+(.[0-9]+)?/, " "],
+        className: { 2: "number", 4: "number" }
+      },
+      {
+        begin: [" ", /[0-9]+(.[0-9]+)?/, "..", /[0-9]+(.[0-9]+)?/, "..", /[0-9]+(.[0-9]+)?/, " "],
+        className: { 2: "number", 4: "number", 6: "number" }
+      }
+    ]
   };
 };
 const diiaSpecial = () => {
@@ -165,67 +177,67 @@ const diiaSpecial = () => {
       keyword:
         "макет субʼєкт це дія рівно більше менше макет пак спробувати містить не я js файл впасти тривала зловити є перебрати структура чекати взяти якщо або і інакше вернути поки як дати модуль пропустити зупинити",
       literal:
-        "так ні пусто текст логічне число список словник словник_м список_м текст_м число_м логічне_м обʼєкт щось []ніщо []щось []текст []логічне []число []список []словник",
+        "так ні пусто текст логічне число список словник словник_м список_м текст_м число_м логічне_м обʼєкт щось []ніщо []щось []текст []логічне []число []список []словник"
     },
     unicodeRegex: true,
     contains: [
       {
         begin: ["дія", /\(/],
-        className: { 0: "keyword" },
+        className: { 0: "keyword" }
       },
       {
         begin: [/[a-zа-яіїє_]/, /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/, /\(/],
-        className: { 1: "title.function", 2: "title.function" },
+        className: { 1: "title.function", 2: "title.function" }
       },
       {
         begin: [/[A-ZА-ЯІЇЄ]/, /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/, /\(/],
-        className: { 1: "title.class", 2: "title.class" },
+        className: { 1: "title.class", 2: "title.class" }
       },
       {
         begin: [" ", /[A-ZА-ЯІЇЄ]/, /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/],
-        className: { 2: "title.class", 3: "title.class" },
+        className: { 2: "title.class", 3: "title.class" }
       },
       {
         begin: [/\[\]/, /[A-ZА-ЯІЇЄ]/, /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/],
-        className: { 2: "title.class", 3: "title.class" },
+        className: { 2: "title.class", 3: "title.class" }
       },
       {
         begin: ["структура ", /[A-ZА-ЯІЇЄ]/, /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/],
-        className: { 2: "title.class", 3: "title.class" },
+        className: { 2: "title.class", 3: "title.class" }
       },
       {
         begin: ["макет ", /[A-ZА-ЯІЇЄ]/, /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/],
-        className: { 2: "title.class", 3: "title.class" },
+        className: { 2: "title.class", 3: "title.class" }
       },
       {
         begin: [/[A-ZА-ЯІЇЄ]/, /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/, /\./],
-        className: { 1: "title.class", 2: "title.class" },
+        className: { 1: "title.class", 2: "title.class" }
       },
       {
         begin: ["дати ", /[A-ZА-ЯІЇЄ]/, /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/],
-        className: { 2: "title.class", 3: "title.class" },
+        className: { 2: "title.class", 3: "title.class" }
       },
       {
         begin: ["дати ", /[a-zа-яіїє_]/, /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ0-9]*/],
-        className: { 2: "title.function", 3: "title.function" },
+        className: { 2: "title.function", 3: "title.function" }
       },
       {
         begin: [
           "взяти ",
           /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ]+\./,
           /[a-zа-яіїє_]/,
-          /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ]*/,
+          /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ]*/
         ],
-        className: { 3: "title.function", 4: "title.function" },
+        className: { 3: "title.function", 4: "title.function" }
       },
       {
         begin: [
           "взяти ",
           /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ]+\./,
           /[A-ZА-ЯІЇЄ]/,
-          /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ]*/,
+          /[a-zA-Zа-яА-ЯіІїЇєЄ_ʼ]*/
         ],
-        className: { 3: "title.class", 4: "title.class" },
+        className: { 3: "title.class", 4: "title.class" }
       },
       {
         className: "string",
@@ -235,22 +247,22 @@ const diiaSpecial = () => {
           {
             begin: /%\(/,
             end: /\)/,
-            scope: "subst",
-          },
-        ],
+            scope: "subst"
+          }
+        ]
       },
       hljs.COMMENT(";;;", ";;;"),
       hljs.COMMENT(";;", "$"),
       {
         scope: "number",
-        begin: /(-?)0ш([АБВГҐДабвгґд0-9])+/,
+        begin: /(-?)0ш([АБВГҐДабвгґд0-9])+/
       },
       {
         scope: "number",
-        begin: /0д([01])+/,
+        begin: /0д([01])+/
       },
-      hljs.C_NUMBER_MODE,
-    ],
+      hljs.C_NUMBER_MODE
+    ]
   };
 };
 
