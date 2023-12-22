26303ecc63127dd7e40b1d4d0a31fe2e33f7a941
Davyd Kohut <davyd.kohut@gmail.com>
2023-12-22 08:41:31 +0200

    🌠

---
+++ pages/план/д.vue
@@ -0,0 +1,15 @@
+<script setup>
+useHead({
+  title: "Д | План | Мавка"
+});
+</script>
+
+<template>
+  <main class="container">
+    <UiNavbar />
+    <h1>Дельтаплан</h1>
+    <p>
+      //
+    </p>
+  </main>
+</template>
