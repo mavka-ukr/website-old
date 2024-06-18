de9f99fbb484aa2ac8ac80deb9bb7224877c9544
Davyd Kohut <davyd.kohut@gmail.com>
2024-06-18 06:49:24 +0300

    🧬

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,6 +39,10 @@ export default defineNuxtConfig({
           rel: "stylesheet",
           href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200",
         },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Noto+Color+Emoji&display=swap",
+        },
       ],
     },
   },
